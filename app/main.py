aa608653d78b03f2d6997f3734c91b4c1a563fc0
datenbaecker <lukas@datenbaecker.ch>
2022-06-14 17:42:52 +0200

    download address data from swisstopo

--- app/main.py
+++ app/main.py
@@ -1,4 +1,9 @@
+import io
+import zipfile
+
+import requests
 from fastapi import FastAPI
+import pandas as pd
 
 
 app = FastAPI()
@@ -8,3 +13,12 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+@app.get("/adresses.parquet")
+async def get_addresses():
+    url = "https://data.geo.admin.ch/ch.swisstopo.amtliches-gebaeudeadressverzeichnis/csv/2056/ch.swisstopo.amtliches-gebaeudeadressverzeichnis.zip"
+    r = requests.get(url)
+    stream = io.BytesIO(r.content)
+    zip_file = zipfile.ZipFile(stream)
+    zip_file.extract("pure_adr.csv", "/data")
+    data = pd.read_csv("/data/pure_adr.csv", sep=";")
+    return list(data.columns)
